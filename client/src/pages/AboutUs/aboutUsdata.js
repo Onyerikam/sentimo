048269e82044412d8ff58f08c5d18eda4788e439
Onyerikam <andrespedrionyerikam@gmail.com>
2022-12-23 23:05:35 +0100

    Updated code

--- client/src/pages/AboutUs/aboutUsdata.js
+++ client/src/pages/AboutUs/aboutUsdata.js
@@ -2,7 +2,7 @@ export const data = {
   godwin: {
     name: "Onyerikam",
     whatWork: "SENTIMO",
-    content: "I am a full time Computer Programmer and Web Developer. I am an individual who likes to be consistent and attain perfection in my work. I worked on the making of Sentimo.",
+    content: "I am a full time Computer Programmer and Web Developer. I am an individual who likes to be consistent and attain perfection in my work. I made this platform, Sentimo.",
     socialLinks: ["mailto:andrespedrionyerikam@gmail.com","https://linkedin.com","https://instagram.com", "https://github.com/Onyerikam", ],
   },
 };
