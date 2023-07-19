c37e979b4f07c45a36c0438729cf82c088ef0d70
Onyerikam <115038141+Onyerikam@users.noreply.github.com>
2023-07-19 14:53:01 +0100

    Change Social Links due to New Profiles

--- client/src/pages/AboutUs/aboutUsdata.js
+++ client/src/pages/AboutUs/aboutUsdata.js
@@ -3,7 +3,7 @@ export const data = {
     name: "Onyerikam",
     whatWork: "SENTIMO",
     content: "I am a full time Computer Programmer and Web Developer. I am an individual who likes to be consistent and attain perfection in my work. I made this platform, Sentimo.",
-    socialLinks: ["mailto:andrespedrionyerikam@gmail.com","https://facebook.com","https://twitter.com/ytechgod", "https://github.com/Onyerikam", ],
+    socialLinks: ["mailto:andrespedrionyerikam@gmail.com","https://facebook.com/MrOGJT","https://twitter.com/MrOGJT", "https://github.com/Onyerikam", ],
   },
 };
 
